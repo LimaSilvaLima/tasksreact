dc97761037f8cc7e30dafadca81663b0a19ee95a
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-07-07 20:34:11 -0300

    rodando a tela iniial completa - configurado tailwindcss

---
+++ src/components/AddTasks.jsx
@@ -0,0 +1,14 @@
+function AddTasks() {
+  return (
+    <div>
+      <h2>Add Tasks</h2>
+      <form>
+        <input type="text" placeholder="Task Name" />
+        <input type="date" />
+        <button type="submit">Add Task</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddTasks;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,11 @@
+function Tasks() {
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <p>List of tasks will go here.</p>
+    </div>
+  );
+}
+
+export default Tasks;
+

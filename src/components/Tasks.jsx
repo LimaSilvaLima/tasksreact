975569f063bb19d4d0b1e5a5dba6b5ad29d762fc
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-07-12 21:19:10 -0300

    implementadas as funçoes de alterar e deletes com respectivos bottuns

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,15 +1,33 @@
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+
 function Tasks(props) {
   
   return (
     <div>
       <div>
-          <h1>{props.tasks.map((task) =>(
-            <p>{task.title}</p>
+          <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">{props.tasks.map((task) =>(
+              <li key={task.id} className="flex gap-2">
+                <button onClick={() =>props.onTaskClick(task.id)}
+                  className={`bg-slate-400 text-left w-full text-white p-2 rounded-md" ${task.isCompleted && "line-through"
+                  }`}
+                >
+                  {task.title}
+                </button>
+                <button className="bg-slate-400 p-2 rounded-md text-white">
+                  <ChevronRightIcon />
+                </button>
+                <button className="bg-slate-400 p-2 rounded-md text-white">
+                  <TrashIcon/>
+                </button>
+              </li>
             ))}
-          </h1>
+          </ul>
       </div>
     </div>
   );
 }
 
 export default Tasks;
+
+//para use icone instalar lucide-icons/react
+//npm install lucide

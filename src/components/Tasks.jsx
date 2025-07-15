918bcd9e96760f87eefcffb2519ef74297f77665
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-07-14 21:36:11 -0300

    rondando com navigate a nova pagina de detalhes da tarefa

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,20 +1,27 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 
 function Tasks({tasks, onDeleteTaskClick, onTaskClick}) {
-  
+  const navigate = useNavigate();
+
+  function onSeeDatailsClick(task) {
+    navigate(`/task?title=${task.title}&description=${task.description}`);
+  }
+
   return (
     <div>
       <div>
           <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">{tasks.map((task) =>(
               <li key={task.id} className="flex gap-2">
-                <button onClick={() => onTaskClick(task.id)}
+                <button onClick={() => onTaskClick(task)}
                   className={`bg-slate-400 text-left w-full text-white p-2 rounded-md" ${task.isCompleted && "line-through"
                   }`}
                 >
                   {task.title}
                 </button>
-                <button className="bg-slate-400 p-2 rounded-md text-white">
+                <button onClick={() => onSeeDatailsClick(task)}
+                className="bg-slate-400 p-2 rounded-md text-white">
                   <ChevronRightIcon />
                 </button>
                 <button onClick={()=> onDeleteTaskClick(task.id)} className="bg-slate-400 p-2 rounded-md text-white">
@@ -36,3 +43,4 @@ export default Tasks;
 //npm install uuid
 //import { v4 as uuidv4 } from 'uuid'; //para gerar id aleatorio
 //const id = uuidv4(); //gerar id aleatorio
+//npm i react-router-dom

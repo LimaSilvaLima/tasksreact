26ae1b4e61d1ffd2b129072de19842f34789ac1e
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-07-16 14:12:55 -0300

    revisdo page 'Detalhes da Tarafe' com retorno a page anterior

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,16 +1,19 @@
 import { ChevronLeftIcon } from "lucide-react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 
 function TaskPage() {
-    const [searchParams] = useSearchParams();
+  const navigate = useNavigate();  
+  const [searchParams] = useSearchParams();
     const title = searchParams.get("title");
     const description = searchParams.get("description");
    return (
      <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
        <div className="w-[500px] space-y-4">
         <div className="flex justify-center relative ">
-          <button className="absolute left-0 top-0 p-2 bottom-0 md mb-6 text-slate-100" onClick={() => window.history.back()}>
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 top-0 p-2 bottom-0 md mb-6 text-slate-100">
             <ChevronLeftIcon/>
           </button>
           <h1 className="text-3xl text-slate-100 font-bold">
@@ -27,3 +30,6 @@ function TaskPage() {
 }
 
 export default TaskPage;
+
+//button voltar pagina anterior
+//<button className="absolute left-0 top-0 p-2 bottom-0 md mb-6 text-slate-100" onClick={() => window.history.back()}>4

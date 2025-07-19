5021a59b61d43ab3f8802d8591c0d1e798419faf
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-07-19 02:31:34 -0300

    aplictivo concluido. Iniciado o processo de dDepoy com 'npm run build'

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Tasks from "./components/Tasks";
 import AddTasks from "./components/AddTasks";
 import {v4} from 'uuid'; 
+import Title from "./components/Title";
 
 
 
@@ -69,9 +70,9 @@ useEffect(()=>{
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">
+        <Title>
           Gerenciador de Tarefas
-        </h1>
+        </Title>
         <AddTasks onAddTaskSubmit={onAddTaskSubmit} />
         <Tasks 
           tasks={tasks}

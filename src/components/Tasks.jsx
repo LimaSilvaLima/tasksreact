0ac55de05ad0c9d3a4bfc0babf28d33d4822e887
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-07-19 03:06:55 -0300

    efutados ajuste, adicionado checkicon

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -20,10 +20,12 @@ function Tasks({tasks, onDeleteTaskClick, onTaskClick}) {
       <div>
           <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">{tasks.map((task) =>(
               <li key={task.id} className="flex gap-2">
-                <button onClick={() => onTaskClick(task)}
-                  className={`bg-slate-400 text-left w-full text-white p-2 rounded-md" ${task.isCompleted && "line-through"
+                <button onClick={() => onTaskClick(task.id)}
+                  className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${
+                    task.isCompleted && "line-through"
                   }`}
                 >
+                  {task.isCompleted && <CheckIcon/>}
                   {task.title}
                 </button>
                 <Button onClick={() => onSeeDatailsClick(task)}>
